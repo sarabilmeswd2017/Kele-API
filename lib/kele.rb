1f92db0bb069b461fdec3d65ae361cccf7fbc103
Sara Bilmes <saraesrig@gmail.com>
2017-09-04 21:25:25 +0300

    Messaging:complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -31,17 +31,36 @@ class Kele
   end
 
   def get_messages(page_number = 1)
-    response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization" => @auth_token })
+    response = self.class.get("https://www.bloc.io/api/v1/message_threads?page=#{page_number}", headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
 
-  def create_message(sender, recipient_id, token = "762d2d62-6e11-45c0-b265-7ae073b59304", subject, stripped_text)
-    response = self.class.post("https://www.bloc.io/api/v1/messages", headers: { "authorization" => @auth_token })
+  def create_message(sender, recipient_id, subject, stripped_text)
+    options = {
+      body: {
+        "sender": sender,
+        "recipient_id": recipient_id,
+        "subject": subject,
+        "stripped-text": stripped_text
+     },
+     headers: { "authorization" => @auth_token }
+   }
+    response = self.class.post("https://www.bloc.io/api/v1/messages", options)
     JSON.parse(response.body)
   end
 
-  def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment)
-    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", headers: { "authorization" => @auth_token })
+  def create_submissions(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollement_id)
+    options = {
+      body: {
+        "checkpoint_id": checkpoint_id,
+        "assignment_branch": assignment_branch,
+        "assignment_commit_link": assignment_commit_link,
+        "comment": comment,
+        "enrollement_id": enrollement_id
+      },
+      headers: { "authorization" => @auth_token }
+    }
+    response = self.class.post("https://www.bloc.io/api/v1/checkpoint_submissions", options)
     JSON.parse(response.body)
   end
 end
